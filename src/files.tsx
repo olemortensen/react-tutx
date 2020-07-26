f32061fa6e5038704daa2ba928eea22c390e9e07
Ole MORTENSEN <ole.mortensen@me.com>
2020-07-26 16:02:20 +0200

    add call to rest service

--- src/files.tsx
+++ src/files.tsx
@@ -1,14 +1,63 @@
 import React from "react";
+import axios from 'axios';
+import {List, ListItem} from "@material-ui/core";
 
-export default function Files()  {
-    return (
-        <div>
-            <ol>
-                <li>file 1</li>
-                <li>file 2</li>
-                <li>file 3</li>
-                <li><a href='/main/42'>file 4</a></li>
-            </ol>
-        </div>
-    );
+interface Item {
+    id: number,
+    userId: number,
+    title: string
+}
+interface State {
+    error: any,
+    isLoaded: boolean,
+    items: any[]
+}
+export default class Files extends React.Component {
+    state: State = {
+        error: null,
+        isLoaded: false,
+        items: []
+    };
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts").then(
+            result => {
+                this.setState({
+                    isLoaded: true,
+                    items: result.data
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            err => {
+                this.setState({
+                    isLoaded: true,
+                    error: err
+                });
+            }
+        );
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        console.log(isLoaded, items)
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <List>
+                    <ListItem key='0'><a href='/main/42'>file 4</a></ListItem>
+                    {items.map(item => (
+                        <ListItem key={item.id}>
+                            {item.id}: {item.title}
+                        </ListItem>
+                    ))}
+                </List>
+            );
+        }
+    }
+
 }
